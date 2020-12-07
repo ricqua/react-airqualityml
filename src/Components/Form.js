b3b1b7a94c90671e1f9b7aca876def4d2b3a39be
Ricqua <ricqua@gmail.com>
2020-12-07 19:43:02 +0900

    used the code provided by azure ml studio.  Still not working

--- src/Components/Form.js
+++ src/Components/Form.js
@@ -21,26 +21,38 @@ const Form = () => {
       "Authorization",
       "Bearer sJ50wZb8uGgV8EFIamZuI+tY5dcpsZ2VkAhgmv+oJq0xF6ASfurz1uaBJ3CVgTlXfn7tjjpK4lgsWeYRKIDoLg=="
     );
+    myHeaders.append("Content-Type", "application/json");
 
-    var raw =
-      '{"Inputs": {"input1": [{"Temperature": 20,"Humidity": 55,"CO2": 800}]},"GlobalParameters": {}}';
+    var raw = JSON.stringify({
+      Inputs: {
+        input1: {
+          ColumnNames: ["Temperature", "Humidity", "CO2"],
+          Values: [
+            ["22", "55", "800"],
+            ["22", "55", "800"],
+          ],
+        },
+      },
+      GlobalParameters: {},
+    });
 
     var requestOptions = {
       method: "POST",
       headers: myHeaders,
       body: raw,
       redirect: "follow",
+      mode: "cors", // no-cors, *cors, same-origin
+      cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+      credentials: "same-origin",
     };
 
     fetch(
-      "https://ussouthcentral.services.azureml.net/workspaces/2925ec637804440e8a968080918b7775/services/63cdf886d22c441085c48b1ac1af75aa/execute?api-version=2.0&format=swagger",
+      "https://ussouthcentral.services.azureml.net/workspaces/2925ec637804440e8a968080918b7775/services/63cdf886d22c441085c48b1ac1af75aa/execute?api-version=2.0&details=true",
       requestOptions
     )
       .then((response) => response.text())
       .then((result) => console.log(result))
       .catch((error) => console.log("error", error));
-
-    console.log("response");
   };
 
   return (
