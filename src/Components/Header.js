c4273765175f388ff88d396be9f9602260bfd434
Ricqua <ricqua@gmail.com>
2021-02-10 18:55:36 +0900

    changed header text

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -6,15 +6,16 @@ const Header = () => {
       <div className="headerComponent">
         <h1 className="headerComponent_title">pm2.5 forecast</h1>
         <p className="headerComponent_subtitle">
-          This web app will using machine learning to predict an expected air
-          pollution value (pm2.5) in particles per million (ppm) based on
-          temperate (ºC), humidity (%RH) and carbon dioxide (CO2) inputs. The
-          machine learning model was trained on data collected between x - y
-          dates in Seoul, South Korea using Arduino controlled weather stations.
-          As a result, the model can only reasonably recognise weather patterns
-          within the range of data collected. These data ranges are indicated in
-          each input field. Azure ML Studio was used to train the model and is
-          currently being used to host the model for this web app.
+          This web app employs a machine learning algorithm to predict the
+          expected air pollution value (pm2.5) in particles per million (ppm)
+          based on temperate (ºC), humidity (%RH) and carbon dioxide (CO2)
+          inputs. The machine learning model was trained on data collected
+          between May 2019 - July 2019 in Seoul, South Korea using Arduino
+          controlled weather stations. As a result, the model can only
+          reasonably recognise weather patterns within the range of data
+          collected. Any data outside the training data ranges will produce a
+          mostly inaccurate forecast. Azure ML Studio was used to train the
+          model and is currently being used to host the model for this web app.
         </p>
       </div>
     </React.Fragment>
